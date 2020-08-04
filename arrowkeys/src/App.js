aba76a0fcab755cee2e7747facec6b9a48584d81
Patton Parham <patton.parham@verizon.net>
2020-08-03 19:05:03 -0500

    responsive animation progress

--- arrowkeys/src/App.js
+++ arrowkeys/src/App.js
@@ -1,62 +1,95 @@
 import React, {useState, useEffect, useRef} from 'react';
+import { useSpring, animated } from 'react-spring';
+
 import logo from './logo.svg';
 import './App.css';
+
 import KeyboardEventHandler from 'react-keyboard-event-handler';
 
-function App(props) {
-const [moveUpBool, setMoveUpBool] = useState(false);
-const [moveDownBool, setMoveDownBool] = useState(false);
-const [moveRightBool, setMoveRightBool] = useState(false);
-const [moveLeftBool, setMoveLeftBool] = useState(false);
-
-let objImage = null;
-objImage = document.getElementById("character");
-objImage.style.position='relative';
-objImage.style.left='0px';
-objImage.style.top='0px';
-
-function moveLeft(event){
-  event.preventDefault();
-  if(moveLeftBool){
-    objImage.style.left=parseInt(objImage.style.left) - 5 +'px';
-  } 
-}
-function moveUp(event){
-  if(moveUpBool){
-    objImage.style.top=parseInt(objImage.style.top) - 5 +'px';
-  }
-}
-function moveRight(event){
-  if(moveRightBool){
-    objImage.style.left=parseInt(objImage.style.left) + 5 +'px';
-  }
-}
-function moveDown(event){
-  if(moveDownBool){
-    objImage.style.top=parseInt(objImage.style.top) + 5 +'px';
-  }
-}
 
 
 
-function Move(event){
+function App() {
 
-}
+  const [isToggled, setToggle] = useState(false);
+
+  const fade = useSpring({
+    // opacity: isToggled ? 1: 0,
+    color: isToggled ? 'black' : 'green',
+    backgroundColor: isToggled ? 'black' : 'green',
+    width: isToggled ? '100px' : '200px',
+    marginTop: isToggled ? '0px' : '400px'
+
+  });
+  console.log(fade);
 
 
   return (
     
-    <div className="App" >
-   <div>key detected: {props.eventKey}</div>
-  <KeyboardEventHandler
-    handleKeys={['up', 'down', 'left', 'right']}
-    
-    onKeyEvent={(key, e) => console.log(`do something upon keydown event of ${key}`)} />
-    <div className="character" id = "character">
-    
+   <animated.div className="App">
+     <div className="appbody">
+     <animated.h1 className="block"> Hi</animated.h1>
+    <animated.div className="block" style={fade}> Hi</animated.div>
+    <button onClick={() => setToggle(!isToggled)}> Toggle </button>
     </div>
-</div>
+  
+
+
+  </animated.div>
   );
 }
 
 export default App;
+// const [moveUpBool, setMoveUpBool] = useState(false);
+// const [moveDownBool, setMoveDownBool] = useState(false);
+// const [moveRightBool, setMoveRightBool] = useState(false);
+// const [moveLeftBool, setMoveLeftBool] = useState(false);
+
+
+
+
+
+
+// function moveLeft(event){
+//   event.preventDefault();
+//   if(moveLeftBool){
+//     objImage.style.left=parseInt(objImage.style.left) - 5 +'px';
+//   } 
+// }
+// function moveUp(event){
+//   if(moveUpBool){
+//     objImage.style.top=parseInt(objImage.style.top) - 5 +'px';
+//   }
+// }
+// function moveRight(event){
+//   if(moveRightBool){
+//     objImage.style.left=parseInt(objImage.style.left) + 5 +'px';
+//   }
+// }
+// function moveDown(event){
+//   if(moveDownBool){
+//     objImage.style.top=parseInt(objImage.style.top) + 5 +'px';
+//   }
+// }
+
+
+// const guy=document.getElementById('guy');
+// const container=document.getElementById('container');
+
+// let guyLeft = 0;
+
+// function anim(e){
+
+// alert(e.keyCode);
+
+// if(e.keyCode==39){
+//     guyLeft +=2;
+//     guy.style.left = guyLeft + 'px';
+// } 
+// if(e.keyCode==37){
+//   guyLeft -=2;
+//   guy.style.left = guyLeft + 'px';
+// } 
+// }
+
+// document.onkeydown = anim;
